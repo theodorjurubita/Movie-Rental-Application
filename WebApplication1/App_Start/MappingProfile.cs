872fa67e44280a93be6cc30b27f036c129657bff
Theodor Stefan Jurubita <theodor.jurubita@gmail.com>
2016-10-06 17:11:44 +0300

    Movie API added

--- WebApplication1/App_Start/MappingProfile.cs
+++ WebApplication1/App_Start/MappingProfile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using AutoMapper;
+using AutoMapper.Internal;
 using WebApplication1.Dtos;
 using WebApplication1.Models;
 
@@ -12,8 +13,10 @@ namespace WebApplication1.App_Start
     {
         public MappingProfile()
         {
-            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Customer, CustomerDto>().ForMember(c => c.Id, opt => opt.Ignore());
             Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<MovieDto, Movie>();
+            Mapper.CreateMap<Movie, MovieDto>().ForMember(m => m.Id, opt => opt.Ignore());
 
         }
     }
--- WebApplication1/Controllers/Api/CustomersController.cs
+++ WebApplication1/Controllers/Api/CustomersController.cs
@@ -25,12 +25,12 @@ namespace WebApplication1.Controllers.Api
             return _context.Customers.ToList().Select(Mapper.Map<Customer,CustomerDto>);
         }
 
-        public CustomerDto GetCustomer(int id)
+        public IHttpActionResult GetCustomer(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if(customer==null)
                 throw  new HttpResponseException(HttpStatusCode.NotFound);
-            return Mapper.Map<Customer, CustomerDto>(customer);
+            return Ok(Mapper.Map<Customer, CustomerDto>(customer));
 
 
         }
